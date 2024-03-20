63180a721880e5ac5a6123067926740f9f357763
Sean Burke <sean111@gmail.com>
2024-03-20 17:04:26 -0400

    added aws sso login userscript

---
+++ aws-sso-login/aws-sso-login.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Auto AWS SSO Login
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       TheLeagueOfOrdinaryGentlemen
+// @match        https://*.awsapps.com/start/*
+// @match        https://device.sso.us-east-1.amazonaws.com/?user_code=*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        window.close
+// @grant        GM_log
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    waitForElement('#cli_verification_btn').then(element => {
+        element.click();
+    })
+
+    waitForElement('#cli_login_button').then(element => {
+        element.click();
+    })
+
+
+    waitForElement('.awsui_header_mx3cw_1459o_226').then(element => {
+        if(element.innerHTML.indexOf('approved') >= 0) {
+            window.close();
+            return;
+        }
+    })
+
+    //https://stackoverflow.com/a/61511955
+    function waitForElement(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+}
+})();
+
